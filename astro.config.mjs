cca69d26bbb672fa9cfcae8ec9bb8c57a1d939ea
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2026-04-14 11:38:39 -0300

    Update site URL in astro.config.mjs, add GitHub Actions workflow for deployment, and create CNAME file for domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://trevizzotecnologia.com.br',
+  site: 'https://trevizzosolucoes.com.br',
   integrations: [sitemap()],
 });
